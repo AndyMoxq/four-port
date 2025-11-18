37b0e77b0f306c391b3f7bdb87a59f36239d3bc1
AndyMoxq <165084870+AndyMoxq@users.noreply.github.com>
2025-11-18 23:39:28 +0800

    Create OceanTracking.php

---
+++ OceanTracking.php
@@ -0,0 +1,30 @@
+<?php
+namespace Tracking\Ocean;
+use Tracking\Ocean\Request\SubscribeRequest;
+use Tracking\Ocean\Response\SubscribeResponse;
+use Tracking\Ocean\Request\GetTrackingRequest;
+use Tracking\Ocean\Response\GetTrackingResponse;
+class OecanTracking{
+    /**
+     * 订阅海运跟踪
+     * @param string $carrierCode
+     * @param string $billNo
+     * @return SubscribeResponse
+     */
+    public static function subscribe(string $carrierCode,string $billNo): SubscribeResponse{
+        $request = new SubscribeRequest();
+        $request -> setTracking($carrierCode,$billNo);
+        return $request->fetch();
+    }
+
+    /**
+     * 获取追踪信息
+     * @param string $subscriptionId
+     * @return GetTrackingResponse
+     */
+    public static function getTracking(string $subscriptionId): GetTrackingResponse{
+        $request = new GetTrackingRequest();
+        $request->setSubscriptionId($subscriptionId);
+        return $request->fetch();
+    }
+}
